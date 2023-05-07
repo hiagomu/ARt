4bcf2cbf620817c91ed69309af4472571f5c8507
Hiago <hiagomuuu@gmail.com>
2023-05-07 17:06:51 -0300

    commit inicial

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVlSclhjUC8vLy8vQUFBQm1YUUcvazdCN0VtWWxpSjZQdlRKSldGQ21NbVFQVzNHUHpST3lLZmNIVmhSemQzTmt1ZGdjNEpwd2k3cEJvUmdWV1dRZi9VQ2QzRlM4SjBnN1NUWTF5bCtHVHNqS3RJcC9vR3NtTWRkYS9lRE5aS1BCRTBmM2lqWnRmSXNTcTRDR082Q0UzRVdMcVc2OEZTY1RPU0YxdGNaSnc2LzR1aCsvTG1leE5NRGNzMEN2dGJCdFd2N1lNa29DNk9McEh1cU9sWUk4YmZoQnI2eTExckxPT0x1RGpQR3hMdTNVL3pzMVhoRzFQSkYybEZqcUltUGJBNmRhK25PMm9mSFNiS21XajFoeHRPN2RnYU1tZnBpL1QvT0tET2pvVW1uOWxTL1o4eElUVkJEcERxdERrSkZ4ZDZhb09ZSUhPRE1LOHJBbU5XY29zSTBYaUJWRDRSNW40TDEvS3dGdlZHdkt3Vll3UTRkemVZUTRYR3I=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
